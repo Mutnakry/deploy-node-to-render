7de18940baf6063d869d142c4bc4561e4b2e310d
Nakry Mut <nakrymut375@gmail.com>
2024-12-26 22:34:04 +0700

    update last code end 4

--- index.js
+++ index.js
@@ -1,55 +1,59 @@
-
 // const express = require('express');
 // const cors = require('cors');
-// const db = require('./src/utile/db');
+// const db = require('./src/utile/db'); 
 // const app = express();
 // const path = require('path');
-// const port = 6700;
+// const port = process.env.PORT || 6700
 // require('dotenv').config();
 
-
-// app.use(express.json()); // Middleware to parse JSON bodies
-// app.use(cors()); // Middleware to handle CORS
-
-
-// // Connect to the database
-// db.connect(err => {
-//     if (err) {
-//         console.error('Error connecting to the database:', err);
-//         return;
-//     }
-//     console.log('Connected to the MySQL database.');
-// });
+// app.use(express.json());
+// app.use(cors());
 
 // // Import and use the category routes
 // const categoryRoutes = require('./src/route/category.route');
 // app.use('/api/categories', categoryRoutes); 
-// // get image static files show in front end
 // app.use('/image', express.static(path.join(__dirname, './src/public/image')));
 
-// // Import and use the category routes
 // const ProductRoutes = require('./src/route/product.router');
 // app.use('/api/product', ProductRoutes); 
 
-// // Import and use the authentication routes
 // const authRoutes = require('./src/route/authRoute');
 // app.use('/api/auth', authRoutes);
+
+// app.get('/api', (req, res) => {
+//     db.query('SELECT * FROM tblcontect', (err, results) => {
+//         if (err) {
+//             console.error('Database error:', err);  // Log the error
+//             return res.status(500).json({ message: 'Internal Server Error' });
+//         }
+//         if (!results.length) {
+//             console.log('No products found.');
+//             return res.status(404).json({ message: 'No products found' });
+//         }
+//         res.json(results);
+//     });
+// });
+
 
 
 // // Start the server
 // app.listen(port, () => {
 //     console.log(`Server is running on port ${port}`);
+//     console.log(`Access the API at http://localhost:${port}/api/product`);
 // });
+
+
+
 
 
 
 
 const express = require('express');
 const cors = require('cors');
-const db = require('./src/utile/db'); // Database connection is already initialized in db.js
+const db = require('./src/utile/db'); 
 const app = express();
 const path = require('path');
-const port = 6700;
+const port = process.env.PORT || 6700
 require('dotenv').config();
 
 app.use(express.json());
@@ -66,8 +70,8 @@ app.use('/api/product', ProductRoutes);
 const authRoutes = require('./src/route/authRoute');
 app.use('/api/auth', authRoutes);
 
-app.get('/api/pro', (req, res) => {
-    db.query('SELECT * FROM product', (err, results) => {
+app.get('/api', (req, res) => {
+    db.query('SELECT * FROM tblcontect', (err, results) => {
         if (err) {
             console.error('Database error:', err);  // Log the error
             return res.status(500).json({ message: 'Internal Server Error' });
