af8a5358191a17ccaf3112d1775f8296f40fdf44
Nakry Mut <nakrymut375@gmail.com>
2024-12-26 22:47:37 +0700

    update last code end

--- index.js
+++ index.js
@@ -1,53 +1,3 @@
-// const express = require('express');
-// const cors = require('cors');
-// const db = require('./src/utile/db'); 
-// const app = express();
-// const path = require('path');
-// const port = process.env.PORT || 6700
-// require('dotenv').config();
-
-// app.use(express.json());
-// app.use(cors());
-
-// // Import and use the category routes
-// const categoryRoutes = require('./src/route/category.route');
-// app.use('/api/categories', categoryRoutes); 
-// app.use('/image', express.static(path.join(__dirname, './src/public/image')));
-
-// const ProductRoutes = require('./src/route/product.router');
-// app.use('/api/product', ProductRoutes); 
-
-// const authRoutes = require('./src/route/authRoute');
-// app.use('/api/auth', authRoutes);
-
-// app.get('/api', (req, res) => {
-//     db.query('SELECT * FROM tblcontect', (err, results) => {
-//         if (err) {
-//             console.error('Database error:', err);  // Log the error
-//             return res.status(500).json({ message: 'Internal Server Error' });
-//         }
-//         if (!results.length) {
-//             console.log('No products found.');
-//             return res.status(404).json({ message: 'No products found' });
-//         }
-//         res.json(results);
-//     });
-// });
-
-
-
-// // Start the server
-// app.listen(port, () => {
-//     console.log(`Server is running on port ${port}`);
-//     console.log(`Access the API at http://localhost:${port}/api/product`);
-// });
-
-
-
-
-
-
-
 const express = require('express');
 const cors = require('cors');
 const db = require('./src/utile/db'); 
@@ -70,8 +20,8 @@ app.use('/api/product', ProductRoutes);
 const authRoutes = require('./src/route/authRoute');
 app.use('/api/auth', authRoutes);
 
-app.get('/api', (req, res) => {
-    db.query('SELECT * FROM tblcontect', (err, results) => {
+app.get('/api/pro', (req, res) => {
+    db.query('SELECT * FROM product', (err, results) => {
         if (err) {
             console.error('Database error:', err);  // Log the error
             return res.status(500).json({ message: 'Internal Server Error' });
